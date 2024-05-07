2c5e37778d60495acc7ef99d221f816f5b6f1d6e
Akalugo David <109105332+rigamortus@users.noreply.github.com>
2024-05-07 19:30:08 +0100

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World! This is my AWS Project!!";
 console.log(hello());
 exports.hello = hello;
 
